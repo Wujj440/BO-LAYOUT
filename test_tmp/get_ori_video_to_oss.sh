469f9db8d80c2857201d0b2ef30580f67089f330
jojo <94116529+Wujj440@users.noreply.github.com>
2026-07-23 13:53:21 +0800

    Update get_ori_video_to_oss.sh

--- test_tmp/get_ori_video_to_oss.sh
+++ test_tmp/get_ori_video_to_oss.sh
@@ -7,7 +7,7 @@ LOG="logs/get_ori_video_$(date +%Y%m%d_%H%M%S).log"
 
 BOX_ID="${BOX_ID:-1420125020341}"
 CAMERA_ID="${CAMERA_ID:-DS-2CD3786G2-IZS20230731AAWRAE4588060}"
-DURATION="${DURATION:-7200}"
+DURATION="${DURATION:-60}"
 
 python3 -m pip install -q opencv-python-headless oss2
 
